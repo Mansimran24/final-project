e4ab35a37eeb90a0d11596f0e5619974225c137e
Mansimran24 <W0786967@myscc.ca>
2022-11-17 04:06:41 -0500

    upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,32 @@
-import {Row, Col} from "antd";
-
+import {Row, Col, Button} from "antd";
+import { useNavigate } from "react-router-dom";
+import {Link} from "react-router-dom";
 export default function Home(){
-    return(
-<>
-        <Row>
-            <Col xl={24}><h1>Home Page</h1></Col>
-        </Row>
 
+    const navigate = useNavigate()
+const handleClick = () =>{
+    console.log("Something Happened")
+    navigate("/login")
+}
+
+    return(
+        
+<>
+<Row style = {{background:"yellow"}}>
+    <ul>
+        <li><Link to="/home">Home</Link></li>
+        <li><Link to="/login">Login</Link></li>
+    </ul>
+</Row>
+        <Row>
+            <Col xl={24}>
+               <h1>Home Page</h1>
+            </Col>
+        </Row>
+       <Row>
+        <Col>
+        <Button type="primary" onClick={handleClick}>trigger</Button></Col>
+       </Row>
         
         </>
         
